fc6db0ca482fc2a2ade88c4b21906cdd237523d0
richardbonneau <richardbonneau91@gmail.com>
2020-11-07 14:20:43 -0500

    starting to lay out countries colors

--- Map/Scripts/MapTile.gd
+++ Map/Scripts/MapTile.gd
@@ -1,8 +1,15 @@
 extends Control
 
-export var country
+export(String,"Neutral", "USA", "USSR", "Europe") var country_this_tile_belongs_to
 
-onready var countries_list = [
-	"USA",
-	"USSR"
-]
+
+func _ready():
+	pass
+	match country_this_tile_belongs_to:
+		"Neutral": $Tile.color = "7a7a7a"
+		"USA": $Tile.color = "1d65d4"
+		"USSR": $Tile.color = "d41d1d"
+		"Europe":$Tile.color = "059cf6"
+
+
+
