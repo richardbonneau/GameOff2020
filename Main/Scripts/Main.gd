bcd28405e74326d2f5e29285ef2133a4d19f25d4
XavierGerD <xaviergervais@gmail.com>
2020-11-08 16:54:15 -0500

    Timer basics

--- Main/Scripts/Main.gd
+++ Main/Scripts/Main.gd
@@ -1,16 +1,21 @@
 extends Node
 
+onready var MainTimer = get_node("MainTimer")
+onready var TimeLeft = get_node("MainTimer/TimeLeft")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	MainTimer.start(600)
 	pass # Replace with function body.
 
+func GetRemainingTime(RemainingTime: float):
+	var RemainingMinutes = str(int(RemainingTime / 60))
+	var RemainingSeconds = int(RemainingTime) % 60
+	var RemainingSecondsText = str(RemainingSeconds) if RemainingSeconds > 10 else "0" + str(RemainingSeconds)		
+	TimeLeft.set_text(str(RemainingMinutes + ":" + RemainingSecondsText))
 
+func _process(delta):
+	GetRemainingTime(MainTimer.time_left)
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
